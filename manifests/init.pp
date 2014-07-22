e11fbaf6a3658165e54d2396928a6ad9ea127e24
Atze de Vries <atze.devries@naturalis.nl>
2014-07-22 13:46:51 +0200

    adduser with java

--- manifests/init.pp
+++ manifests/init.pp
@@ -58,7 +58,7 @@ class nda (
   #}
 
   exec {'create jboss admin user':
-    command    => "/usr/bin/java --silent -jar /opt/jboss/jboss-modules.jar -mp /opt/jboss/modules org.jboss.as.domain-add-user ndaadmin ${admin_password}",
+    command    => "/usr/bin/java -jar /opt/jboss/jboss-modules.jar -mp /opt/jboss/modules org.jboss.as.domain-add-user --silent ndaadmin ${admin_password}",
     unless     => '/bin/cat /opt/jboss/standalone/configuration/mgmt-users.properties | grep ndaadmin',
     environment => 'JBOSS_HOME="/opt/jboss"',
   }
